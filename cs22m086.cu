12a145c79570b589f0b7f8f5585226e623e8baa2
Sudhanshu Gautam <87118093+Sudhanshu2705@users.noreply.github.com>
2023-08-12 00:10:02 +0530

    Add files via upload

---
+++ cs22m086.cu
@@ -0,0 +1,162 @@
+#include<iostream>
+#include<sys/time.h>
+#include<cuda.h>
+using namespace std;
+
+
+__global__ void matrixMultiplication(int p,int q,int r,int *d_A,int *d_B,int *d_E,int transpose){
+    __shared__ int s_A[32];
+    __shared__ int s_B[32];
+    __shared__ int t;
+    t = transpose;
+    int thIdx,thIdy;
+	int temp=0;
+    thIdx = blockIdx.x*blockDim.x + threadIdx.x;
+    thIdy = blockIdx.y*blockDim.y + threadIdx.y;
+	
+    if(thIdx<p && thIdy<r){
+        for(int i=0;i<q;i++){
+			if(threadIdx.y==0)
+				s_A[threadIdx.x] = d_A[blockIdx.x * blockDim.x * q + threadIdx.x*q + i];
+			if(threadIdx.x==0){
+				if(t==1){
+                	s_B[threadIdx.y] = d_B[blockIdx.y * blockDim.y * q + i + threadIdx.y*q];
+            	}
+            	else{
+                	s_B[threadIdx.y] = d_B[blockIdx.y * blockDim.y  + i*r + threadIdx.y];
+            	}
+			}
+			
+            temp +=  s_A[threadIdx.x]* s_B[threadIdx.y];
+            __syncthreads();
+        }
+    }
+	d_E[thIdx*r + thIdy] +=  temp;
+}
+
+// function to compute the output matrix
+void computE(int p, int q, int r, int *h_matrixA, int *h_matrixB, 
+	         int *h_matrixC, int *h_matrixD, int *h_matrixE){
+	// Device variables declarations...
+	int *d_matrixA, *d_matrixB, *d_matrixC, *d_matrixD, *d_matrixE;
+	// allocate memory...
+	cudaMalloc(&d_matrixA, p * q * sizeof(int));
+	cudaMalloc(&d_matrixB, q * r * sizeof(int));
+	cudaMalloc(&d_matrixC, p * q * sizeof(int));
+	cudaMalloc(&d_matrixD, r * q * sizeof(int));
+	cudaMalloc(&d_matrixE, p * r * sizeof(int));
+
+	// copy the values...
+	cudaMemcpy(d_matrixA, h_matrixA, p * q * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_matrixB, h_matrixB, q * r * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_matrixC, h_matrixC, p * q * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_matrixD, h_matrixD, r * q * sizeof(int), cudaMemcpyHostToDevice);
+
+	/* ****************************************************************** */
+	/* Write your code here */
+	/* Configure and launch kernels */
+	int grid_x,grid_y;
+	grid_x = ceil(float(p) / 32);
+	grid_y = ceil(float(r) / 32);
+	dim3 grid3(grid_x,grid_y,1);
+    dim3 block3(32,32,1);
+	matrixMultiplication<<<grid3,block3>>>(p,q,r,d_matrixA,d_matrixB,d_matrixE,0);
+    matrixMultiplication<<<grid3,block3>>>(p,q,r,d_matrixC,d_matrixD,d_matrixE,1);
+    cudaDeviceSynchronize();
+	/* ****************************************************************** */
+
+	// copy the result back...
+	cudaMemcpy(h_matrixE, d_matrixE, p * r * sizeof(int), cudaMemcpyDeviceToHost);
+
+	// deallocate the memory...
+	cudaFree(d_matrixA);
+	cudaFree(d_matrixB);
+	cudaFree(d_matrixC);
+	cudaFree(d_matrixD);
+	cudaFree(d_matrixE);
+}
+
+// function to read the input matrices from the input file
+void readMatrix(FILE *inputFilePtr, int *matrix, int rows, int cols) {
+	for(int i=0; i<rows; i++) {
+		for(int j=0; j<cols; j++) {
+			fscanf(inputFilePtr, "%d", &matrix[i*cols+j]);
+		}
+	}
+}
+
+// function to write the output matrix into the output file
+void writeMatrix(FILE *outputFilePtr, int *matrix, int rows, int cols) {
+	for(int i=0; i<rows; i++) {
+		for(int j=0; j<cols; j++) {
+			fprintf(outputFilePtr, "%d ", matrix[i*cols+j]);
+		}
+		fprintf(outputFilePtr, "\n");
+	}
+}
+
+int main(int argc, char **argv) {
+	// variable declarations
+	int p, q, r;
+	int *matrixA, *matrixB, *matrixC, *matrixD, *matrixE;
+	struct timeval t1, t2;
+	double seconds, microSeconds;
+
+	// get file names from command line
+	char *inputFileName = argv[1];
+	char *outputFileName = argv[2];
+
+	// file pointers
+	FILE *inputFilePtr, *outputFilePtr;
+    
+    inputFilePtr = fopen(inputFileName, "r");
+	if(inputFilePtr == NULL) {
+	    printf("Failed to open the input file.!!\n"); 
+		return 0;
+	}
+
+	// read input values
+	fscanf(inputFilePtr, "%d %d %d", &p, &q, &r);
+
+	// allocate memory and read input matrices
+	matrixA = (int*) malloc(p * q * sizeof(int));
+	matrixB = (int*) malloc(q * r * sizeof(int));
+	matrixC = (int*) malloc(p * q * sizeof(int));
+	matrixD = (int*) malloc(r * q * sizeof(int));
+	readMatrix(inputFilePtr, matrixA, p, q);
+	readMatrix(inputFilePtr, matrixB, q, r);
+	readMatrix(inputFilePtr, matrixC, p, q);
+	readMatrix(inputFilePtr, matrixD, r, q);
+
+	// allocate memory for output matrix
+	matrixE = (int*) malloc(p * r * sizeof(int));
+
+	// call the compute function
+	gettimeofday(&t1, NULL);
+	computE(p, q, r, matrixA, matrixB, matrixC, matrixD, matrixE);
+	cudaDeviceSynchronize();
+	gettimeofday(&t2, NULL);
+
+	// print the time taken by the compute function
+	seconds = t2.tv_sec - t1.tv_sec;
+	microSeconds = t2.tv_usec - t1.tv_usec;
+	printf("Time taken (ms): %.3f\n", 1000*seconds + microSeconds/1000);
+
+	// store the result into the output file
+	outputFilePtr = fopen(outputFileName, "w");
+	writeMatrix(outputFilePtr, matrixE, p, r);
+
+	// close files
+	fclose(inputFilePtr);
+	fclose(outputFilePtr);
+
+	// deallocate memory
+	free(matrixA);
+	free(matrixB);
+	free(matrixC);
+	free(matrixD);
+	free(matrixE);
+
+	return 0;
+}
+	
